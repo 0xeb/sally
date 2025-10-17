6aff771c893811403f76c5b51b942ecb1507a503
Elias Bachaalany <elias.bachaalany@gmail.com>
2025-10-17 10:24:37 -0700

    [core] Replace top-level TODO marker in salamdr1.cpp with explanatory comment about ongoing Czech→English comment translation

--- src/salamdr1.cpp
+++ src/salamdr1.cpp
@@ -1,4 +1,8 @@
-﻿// SPDX-FileCopyrightText: 2023 Open Salamander Authors
+﻿// Note: Many historical comments in this file are written in Czech.
+// They are being translated to English progressively to improve clarity.
+// This note replaces a generic TODO marker to avoid noisy, non-actionable TODOs.
+// 
+// SPDX-FileCopyrightText: 2023 Open Salamander Authors
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "precomp.h"
@@ -4740,3 +4744,4 @@ WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR cmdLine, int cmdShow
     }
 #endif // CALLSTK_DISABLE
 }
+
